3dd77dc1ea08dc9638025f62bafc45ed56ea2280
Oscar Gonzalez <ogonbon@gmail.com>
2025-05-28 18:49:13 -0600

    Se implemento el poder listar

---
+++ SistemaEstudiantes/src/main/java/personal/dao/EstudianteDAO.java
@@ -0,0 +1,54 @@
+package personal.dao;
+import personal.dominio.Estudiante;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static personal.conexion.Conexion.obtenerConexion;
+
+//DAO -> Data Access Object
+public class EstudianteDAO {
+
+    public List<Estudiante> listarEstudiantes(){
+        List<Estudiante> estudiantes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = obtenerConexion();
+        String sql = "SELECT * FROM estudiante ORDER BY id_estudiante";
+
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("id_estudiante"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                estudiantes.add(estudiante);
+            }
+        } catch (SQLException e) {
+            System.out.println("Ocurrio un error al selecionar datos: " + e.getMessage());
+        }
+        //Cerrar conexion a DB
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Ocurrio un error al cerrar la DB: " + e.getMessage());
+            }
+        }
+        return estudiantes;
+    }
+
+    public static void main(String[] args) {
+        var estudianteDAO = new EstudianteDAO();
+        //Listar Estudiantes
+        System.out.println("Listado Estudiantes: ");
+        List<Estudiante> estudiantes= estudianteDAO.listarEstudiantes();
+        estudiantes.forEach(System.out::println);
+    }
+}
